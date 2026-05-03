e167db65424d1c19c9a0e9f3b6e36f8afa9e9848
RenzoTeves <renzo.teves15@gmail.com>
2026-05-02 18:01:33 -0700

    add card

---
+++ app/components/Card.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div className='banner'>
+        <div className='starter'>
+         <img src='https://tavitsphotography.com/wp-content/uploads/2023/07/JNF-114-2-1080x721.jpg.webp'/>    
+        </div>
+    </div>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Card } from "./components/Card";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-        
+    <div>
+        <Card/>
+         <Card/>
+          <Card/>
     </div>
   );
 }
