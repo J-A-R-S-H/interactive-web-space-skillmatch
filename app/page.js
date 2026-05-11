211a8d94589002274b872b32c2e36770d24f14d0
Sarah Samuel <sarahsamuel@Mac.lan>
2026-05-11 08:49:51 -0700

    color update

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Card } from "./components/Card";
 import FlippingCards from "./components/FlippingCards/FlippingCards";
 import SlidingChips from "./components/SlidingChips/SlidingChips";
 
